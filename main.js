91a5a2501f22758af53eb801a0b1a7161e85a31c
Billy Koech <bkkoech@college.harvard.edu>
2017-09-22 19:29:52 +0100

    everything

--- main.js
+++ main.js
@@ -39,11 +39,55 @@ $.ajax({
 
 
 })
+console.log(dataSheets);
 
-var arrayValues = data.values
-for (var i = 0; i < arrayValues.length; i++){
-	var article = $("<article id = art" + i.toString() + "><div> </div><article>");
-	$("#main").append(article);
+input = dataSheets.values.reverse();
+console.log(input);
+var rows = dataSheets.values.length
+for (var i = 0; 0 < rows; i++){
+	var id = i.toString()
+	var article = $("<article class = 'post' id = art" + id + ">" + 
+		            	"<header id = hd" + id + ">" + 
+		            		"<div class = 'title'>" + 
+		           				 "<h2 id = h" + id + "></h2>" +
+		            				"<p id = sub" + id + "></p>" +
+		            		"</div>" + 
+		            		"<div class = 'meta'>" +
+		            			"<time class = 'published' id = t" + id + "></time>" +
+                                "<a href='#' class='author' id = a1" + id + ">" +
+                                	"<span class='name' id=name" + id + ">" + "</span>" +
+                                	 "<img src='images/avatar.jpg' alt='' id = img1" + id + "/>" + 
+                                "</a>" +
+							"</div>" +
+						"</header>" +
+						"<a href='#' class='image featured' id = a2" + id + ">" +
+							"<img src='" + input[i][8] + "' alt='' id = img2" + id + "/>" +
+						"</a>" +
+						"<p id = p" + id + "></p>" +
+						"<footer>" + id +
+							"<ul class='actions'>" +
+								"<li><a href='#'' class='button big'>Continue Reading</a></li>" +
+							"</ul>" +
+							"<ul class='stats'>" +
+								"<li><a href='#' id = topic" + id + "></a></li>" +
+								"<li><a href='#' class='icon fa-heart'>28</a></li>" +
+								"<li><a href='#' class='icon fa-comment'>128</a></li>" +
+							"</ul>" +
+						"</footer>" +
+					"</article>");
+
+
+	
+	$("#mainposts").append(article);
+	$("#name" + id).append(input[i][2]);
+	$("#t" + id).append(input[i][3]);
+	$("#h" + id).append(input[i][4]);
+	$("#sub" + id).append(input[i][5]);
+	$("#p" + id).append(input[i][6]);
+	$("#topic" + id).append(input[i][7]);
+
+
+	console.log(typeof input[i][4], input[i][4]);
 };
 
 
