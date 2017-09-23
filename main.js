ba702a91db070ccf4963fff1e41b1ab62c2f18f0
27grace <27grace@gmail.com>
2017-09-23 10:13:42 +0100

    continue load

--- main.js
+++ main.js
@@ -41,58 +41,71 @@ $.ajax({
 })
 console.log(dataSheets);
 
-
 input = dataSheets.values.reverse();
 console.log(input);
-var rows = dataSheets.values.length
+var numberofposts = dataSheets.values.length
+var poststart = 0, postend = 5
+render()
 
+// Load more when btn clicked 
+$("#load").click(function(){
+	$("#mainposts").empty();
+	if (postend+5<numberofposts){
+		poststart+=5 
+		postend+=5
+		render()
+	} else {
+		poststart=postend
+		postend=numberofposts
+		render()
+		$("#load").addClass('disabled')
+	}
+	console.log('A', poststart, 'B', postend);
+})
 
-for (var i = 0; i < rows; i++){
-	var id = i.toString()
-	var article = $("<article class = 'post' id = art" + id + ">" + 
-		            	"<header id = hd" + id + ">" + 
-		            		"<div class = 'title'>" + 
-		           				 "<h2 id = h" + id + "></h2>" +
-		            				"<p id = sub" + id + "></p>" +
-		            		"</div>" + 
-		            		"<div class = 'meta'>" +
-		            			"<time class = 'published' id = t" + id + "></time>" +
-                                "<a href='#' class='author' id = a1" + id + ">" +
-                                	"<span class='name' id=name" + id + ">" + "</span>" +
-                                	 "<img src='images/avatar.jpg' alt='' id = img1" + id + "/>" + 
-                                "</a>" +
-							"</div>" +
-						"</header>" +
-						"<a href='#' class='image featured' id = a2" + id + ">" +
-							"<img src='" + input[i][8] + "' alt='' id = img2" + id + "/>" +
-						"</a>" +
-						"<p id = p" + id + "></p>" +
-						"<footer id = f" + id + ">" +
-							"<ul class='actions'>" +
-								"<li><a href='#'' class='button big'>Continue Reading</a></li>" +
-							"</ul>" +
-							"<ul class='stats'>" +
-								"<li><a href='#' id = topic" + id + "></a></li>" +
-								"<li><a href='#' class='icon fa-heart'>28</a></li>" +
-								"<li><a href='#' class='icon fa-comment'>128</a></li>" +
-							"</ul>" +
-						"</footer>" +
-					"</article>");
-
-
-	
-	$("#mainposts").append(article);
-	$("#name" + id).append(input[i][2]);
-	$("#t" + id).append(input[i][3]);
-	$("#h" + id).append(input[i][4]);
-	$("#sub" + id).append(input[i][5]);
-	$("#p" + id).append(input[i][6]);
-	$("#topic" + id).append(input[i][7]);
-
-
-	console.log(typeof input[i][4], input[i][4]);
+// Function to render posts from int poststart to intpostend
+function render(){
+	console.log('render', poststart, postend)
+	for (var i = poststart; i < postend; i++){
+		var id = i.toString()
+		var article = $("<article class = 'post' id = art" + id + ">" + 
+							"<header id = hd" + id + ">" + 
+								"<div class = 'title'>" + 
+									 "<h2 id = h" + id + "></h2>" +
+										"<p id = sub" + id + "></p>" +
+								"</div>" + 
+								"<div class = 'meta'>" +
+									"<time class = 'published' id = t" + id + "></time>" +
+									"<a href='#' class='author' id = a1" + id + ">" +
+										"<span class='name' id=name" + id + ">" + "</span>" +
+										 "<img src='images/avatar.jpg' alt='' id = img1" + id + "/>" + 
+									"</a>" +
+								"</div>" +
+							"</header>" +
+							"<a href='#' class='image featured' id = a2" + id + ">" +
+								"<img src='" + input[i][8] + "' alt='' id = img2" + id + "/>" +
+							"</a>" +
+							"<p id = p" + id + "></p>" +
+							"<footer id = f" + id + ">" +
+								"<ul class='actions'>" +
+									"<li><a href='#'' class='button big'>Continue Reading</a></li>" +
+								"</ul>" +
+								"<ul class='stats'>" +
+									"<li><a href='#' id = topic" + id + "></a></li>" +
+									"<li><a href='#' class='icon fa-heart'>28</a></li>" +
+									"<li><a href='#' class='icon fa-comment'>128</a></li>" +
+								"</ul>" +
+							"</footer>" +
+						"</article>");	
+		$("#mainposts").append(article);
+		$("#name" + id).append(input[i][2]);
+		$("#t" + id).append(input[i][3]);
+		$("#h" + id).append(input[i][4]);
+		$("#sub" + id).append(input[i][5]);
+		$("#p" + id).append(input[i][6]);
+		$("#topic" + id).append(input[i][7]);
+	}
 }
-
 
 
 
