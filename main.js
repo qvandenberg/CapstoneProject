573e80f645469b32785bc89bea6aeb41de7c5fa0
qvandenberg <quincy.vandenberg@physics.ox.ac.uk>
2017-09-23 15:43:38 +0100

    Repaired next/prev buttons

--- main.js
+++ main.js
@@ -44,41 +44,65 @@ console.log(dataSheets);
 input = dataSheets.values.reverse();
 console.log(input);
 var numberofposts = dataSheets.values.length
-var poststart = 0, postend = 5
+var poststart = 0, postNum = 5, postend = postNum
 render()
 
-// Load more when btn clicked 
-$("#load").click(function(){
-	$("#mainposts").empty();
-	if (postend+5<numberofposts){
-		poststart+=5 
-		postend+=5
-		render()
-	} else {
-		poststart=postend
-		postend=numberofposts
-		render()
-		$("#load").addClass('disabled')
-	}
-	console.log('A', poststart, 'B', postend);
-})
+// Load next when btn clicked
+$("#loadnext").click(function(){
+    $("#mainposts").empty();
+    if (postend+postNum <numberofposts){
+        poststart+=postNum;
+        postend+=postNum;
+				$("#loadprev").removeClass('disabled');
+				$("#loadnext").removeClass('disabled');
+        render();
+    } else {
+        poststart=numberofposts-postNum;
+        postend=numberofposts;
+        $("#loadnext").addClass('disabled');
+        $("#loadprev").removeClass('disabled');
+				render();
+    }
+    console.log('A', poststart, 'B', postend);
+});
+
+// Load previous when btn clicked
+$("#loadprev").click(function(){
+    $("#mainposts").empty();
+    if (poststart-postNum > 0){
+        poststart -= postNum
+        postend -= postNum
+				$("#loadprev").removeClass('disabled');
+				$("#loadnext").removeClass('disabled');
+        render()
+    } else {
+        poststart=0
+        postend=postNum
+        render()
+        $("#loadprev").addClass('disabled')
+        $("#loadnext").removeClass('disabled')
+    }
+    console.log('A', poststart, 'B', postend);
+});
+
+
 
 // Function to render posts from int poststart to intpostend
 function render(){
 	console.log('render', poststart, postend)
 	for (var i = poststart; i < postend; i++){
 		var id = i.toString()
-		var article = $("<article class = 'post' id = art" + id + ">" + 
-							"<header id = hd" + id + ">" + 
-								"<div class = 'title'>" + 
+		var article = $("<article class = 'post' id = art" + id + ">" +
+							"<header id = hd" + id + ">" +
+								"<div class = 'title'>" +
 									 "<h2 id = h" + id + "></h2>" +
 										"<p id = sub" + id + "></p>" +
-								"</div>" + 
+								"</div>" +
 								"<div class = 'meta'>" +
 									"<time class = 'published' id = t" + id + "></time>" +
 									"<a href='#' class='author' id = a1" + id + ">" +
 										"<span class='name' id=name" + id + ">" + "</span>" +
-										 "<img src='images/avatar.jpg' alt='' id = img1" + id + "/>" + 
+										 "<img src='images/avatar.jpg' alt='' id = img1" + id + "/>" +
 									"</a>" +
 								"</div>" +
 							"</header>" +
@@ -95,7 +119,7 @@ function render(){
 									"<li><a href='#' class='icon fa-comment'>128</a></li>" +
 								"</ul>" +
 							"</footer>" +
-						"</article>");	
+						"</article>");
 		$("#mainposts").append(article); //article
 		$("#name"+id).append(input[i][2]); //author name
 		$("#t"+id).append(input[i][3]); //title
@@ -103,20 +127,17 @@ function render(){
 		$("#sub"+id).append(input[i][5]); //subheading
 		$("#p"+id).append(input[i][6]); //paragraph
 		$("#topic"+id).append(input[i][7]); //topic
-		
+
 		//inserting image or video
 		console.log(input[i][10])
 		if (input[i][10] == "IMAGE"){
 			$("#a2"+id).append("<img src='"+input[i][8]+"' alt='' />")
-		} else if (input[i][10] == "VIDEO") {
-		//	swap watch?v= with embed/ in input[i][8]
-		input[i][8]=input[i][8].replace("watch?v=", "embed/");
-		console.log(input[i][8])
-			$("#a2"+id).append("<iframe width='840' height='500' src='"+input[i][8]+"'></iframe>")
 		}
-		
+		else if (input[i][10] == "VIDEO") {
+			//	swap watch?v= with embed/ in input[i][8]
+			input[i][8]=input[i][8].replace("watch?v=", "embed/");
+			console.log(input[i][8])
+				$("#a2"+id).append("<iframe width='840' height='500' src='"+input[i][8]+"'></iframe>")
+		}
 	}
 }
-
-
-
